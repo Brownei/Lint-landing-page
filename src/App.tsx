ad826a09ab0ee9922b3eb8ae613f7e1283e7bf81
Brownson <esitibrownson@gmail.com>
2024-02-25 23:30:34 +0100

    changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-pretty'>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '960px',
+      md: '1024px',
+      lg: '1440px',
+      xl: '1820px',
+    },
+    extend: {
+      colors: {
+        'lightDarkishBlue': '#0006B1',
+        'purplishBlue': '#E6E6F7',
+        'veryDarkGrey': '#060A0A',
+        'yellow': '#FFF6E6',
+        'purple': '#F0E6F7',
+        'darkPurplishBlue': '#B0B2E7',
+        'lighterPurple': '#D2B0E7',
+        'orange': '#FFA500',
+        'veryDarkBlue': '00034A',
+      }
+    },
+  },
+  plugins: [],
+}
+
